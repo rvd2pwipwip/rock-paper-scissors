b7315d0043a4f0d464cd660ff135c24f8a491376
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-07-11 06:55:42 -0400

    Refactor game with UI and DOM methods to display score

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
 const SELECTION = ['Rock', 'Paper', 'Scissors'];
-const ROUNDS = 5;
+const ROUNDS = 5; // how many rounds in a game
+
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.results');
 
 let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
+let roundsPlayed = 0;
+let gameSummary = '';
 
 // select random item from SELECTION
 function computerPlay() {
   return SELECTION[Math.floor(Math.random() * SELECTION.length)];
 }
 
-function scoreRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === SELECTION[0]) {
     switch (computerSelection) {
       case SELECTION[0]:
@@ -50,35 +55,56 @@ function scoreRound(playerSelection, computerSelection) {
   }
 }
 
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const roundResult = document.createElement('p');
+    roundResult.textContent = playRound(e.target.innerHTML, computerPlay());
+    display.appendChild(roundResult);
+    roundsPlayed++;
+    // disable game buttons after multiple of 5 rounds
+    if (roundsPlayed % ROUNDS === 0) {
+      buttons.forEach((btn) => {
+        btn.disabled = true;
+      });
+      // calculate  and display final score
+      scoreGame();
+      // add replay button to reset game and reanable game buttons
+      const replay = document.createElement('button');
+      replay.innerHTML = 'Replay';
+      replay.addEventListener('click', () => {
+        gameReset();
+        buttons.forEach((btn) => {
+          btn.disabled = false;
+        });
+      });
+      display.appendChild(replay);
+    }
+  });
+});
+
 function scoreGame() {
-  console.log('FINAL SCORE');
-  console.log(`You: ${playerScore}`);
-  console.log(`Computer: ${computerScore}`);
-  console.log(`Ties: ${ties}`);
   if (playerScore > computerScore) {
-    console.log(`You win the game ${playerScore}-${computerScore}!!!`);
+    gameSummary = `You win the game ${playerScore}-${computerScore}!!!`;
   } else if (computerScore > playerScore) {
-    console.log(
-      `You lose... ${computerScore}-${playerScore} for the computer`
-    );
+    gameSummary = `You lose... ${computerScore}-${playerScore} for the computer`;
   } else {
-    console.log(`The game is tied!`);
+    gameSummary = `The game is tied!`;
   }
+  const finalScore = document.createElement('div');
+  finalScore.innerHTML = `<h3>FINAL SCORE</h3>
+    <p>You: ${playerScore}</p>
+    <p>Computer: ${computerScore}</p>
+    <p>Ties: ${ties}</p>
+    <h4>${gameSummary}</h4>
+    `;
+
+  display.appendChild(finalScore);
 }
 
-function game(rounds) {
-  for (let i = 0; i < rounds; i++) {
-    const computerSelection = computerPlay();
-
-    const input = prompt('Rock, paper or scissors?');
-    // parse input into a lower case word with a capitalized first character
-    const playerSelection = `${input[0].toUpperCase()}${input
-      .substring(1)
-      .toLocaleLowerCase()}`;
-
-    console.log(scoreRound(playerSelection, computerSelection));
-  }
-  scoreGame();
+function gameReset() {
+  display.innerHTML = '';
+  playerScore = 0;
+  computerScore = 0;
+  ties = 0;
+  roundsPlayed = 0;
 }
-
-game(ROUNDS);
